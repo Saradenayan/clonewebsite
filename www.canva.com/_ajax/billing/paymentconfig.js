da647d486fa677574b43ab0faa0e8a4dc570faed
Saradenayan <118213104+Saradenayan@users.noreply.github.com>
2023-07-22 14:55:00 +0530

    my code

---
+++ www.canva.com/_ajax/billing/paymentconfig.js
@@ -0,0 +1,1 @@
+'"])}while(1);</x>//{"B":[{"A?":"P","Hz":["B","D","C"],"H0":[],"A":{"A?":"A","A":"B","B":"Kl6dMsyLxsOp7Q2JRgS840fB4vSpt3hiSeKmaVeA0D8=","C":"20230722085846","D":"7619234544918089","E":"https://www.canva.com","F":"PCI"},"B":[],"C":[],"D":["E","D"]}]}
---
+++ www.canva.com/_worker/service/web/service_worker.js
@@ -0,0 +1,47 @@
+self['__canva_public_path__'] = 'https:\/\/static.canva.com\/web/';
+self['bootstrap'] = {
+    page: {
+        "A?": "B",
+        "A": {
+            "A?": "C",
+            "b": "https:\u002F\u002Fdfe5e84dbc214418801081ab37e58da5@o13855.ingest.sentry.io\u002F5546683",
+            "d": null,
+            "h": null,
+            "c": 0.2,
+            "e": null,
+            "f": false,
+            "g": "A",
+            "i": [],
+            "j": {
+                "A": true
+            },
+            "k": {
+                "A": true,
+                "B": 100,
+                "C": 30,
+                "D": 10
+            },
+            "l": []
+        },
+        "H": "A",
+        "I": "WEB",
+        "J": "BROWSER",
+        "K": "N",
+        "L": "N",
+        "G": "N",
+        "F": null,
+        "M": [],
+        "O": [],
+        "N": null,
+        "P": 2000,
+        "a": "https:\u002F\u002Fstatic.canva.com\u002Fweb",
+        "b": null
+    }
+};
+importScripts("https:\/\/static.canva.com\/static\/lib\/sentry\/7.16.0.min.js");
+Sentry.init({
+    "dsn": "https:\u002F\u002Fdfe5e84dbc214418801081ab37e58da5@o13855.ingest.sentry.io\u002F5546683",
+    "release": "20230719-22-bca8e7f",
+    "environment": "prod"
+});
+importScripts('https://static.canva.com/web/5620f0882fe08ac7.runtime.js', 'https://static.canva.com/web/07f048769e0a347b.vendor.js', 'https://static.canva.com/web/c538d5dcd135fd75.vendor.js', 'https://static.canva.com/web/hjfept.5f619189625ca380.js', 'https://static.canva.com/web/65fa9fd5030b064f.vendor.js', 'https://static.canva.com/web/b242f3fd342b8d52.js', 'https://static.canva.com/web/b7e6e2642477efdc.js', 'https://static.canva.com/web/bc8b1df41a190f57.vendor.js', 'https://static.canva.com/web/f65ceb70457d3c45.js');
